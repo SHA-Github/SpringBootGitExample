0c65990730c0b11bc7e0c47664fc1819af0bc38e
LENOVO <LENOVO@SHA>
2020-07-23 19:36:53 +0530

    WH-1111-First Time File Update

---
+++ src/main/java/com/nit/demo/git/SpringBootGitExampleApplication.java
@@ -0,0 +1,14 @@
+package com.nit.demo.git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGitExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootGitExampleApplication.class, args);
+		System.out.println("Welcome to Spring Boot Starter class");
+	}
+
+}
---
+++ src/test/java/com/nit/demo/git/SpringBootGitExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nit.demo.git;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootGitExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
