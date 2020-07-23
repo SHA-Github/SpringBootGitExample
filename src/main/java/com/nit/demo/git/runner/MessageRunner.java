63d2b82579c1590ca83bd082bbe9c5e84228afb3
SHA <SHA@gmail.com>
2020-07-23 20:22:10 +0530

    WH-4444 updated 4th Time

--- src/main/java/com/nit/demo/git/runner/MessageRunner.java
+++ src/main/java/com/nit/demo/git/runner/MessageRunner.java
@@ -1,16 +1,26 @@
 package com.nit.demo.git.runner;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 @Component
 public class MessageRunner implements CommandLineRunner {
-
+private static Logger logger=LoggerFactory.getLogger(MessageRunner.class);
 	@Override
 	public void run(String... args) throws Exception {
+		logger.info("STARTED");
 		System.out.println("Welcome to Spring Boot Runner");
 		System.out.println("Welcome to Spring Boot Git Demo");
-		int a=10;
-		System.out.println("a="+a);
+		try {
+			int a=10;
+			System.out.println("a="+a);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		logger.info("END");
 	}
 
 }
